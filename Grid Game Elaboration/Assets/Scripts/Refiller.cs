f6da570b2ff89c16e8c50266f7a59a2ee1cf3273
Adam Goren <amg1090@nyu.edu>
2019-04-23 03:32:28 -0400

    Game built in folder

---
+++ Grid Game Elaboration/Assets/Scripts/Refiller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Refiller : MonoBehaviour
+{
+    public static GameObject[] refillRow;
+    public GameObject[] gemColors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        refillRow = new GameObject[5];
+        for (int x = 0; x < 5; x++)
+        {
+            refillRow[x] = gemColors[Random.Range(0, gemColors.Length)];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
